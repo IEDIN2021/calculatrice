3427bd6a0202725e35e1b0ad281ca997a486cc53
I.Edin <85932847+IEDIN2021@users.noreply.github.com>
2021-07-29 05:46:03 +0200

    updates de c emaitn

--- edo.js
+++ edo.js
@@ -1,7 +1,7 @@
 function num1num2(){
 	// test de la list
 	//let num1num2;
-	console.log("edin")
+	console.log("work")
 	let num1 = document.getElementById("getToNumber").value; 
 	let num2 = document.getElementById("getToNumber1").value; 
 	let add = +num1 + +num2;
@@ -15,3 +15,43 @@ function num1num2(){
 	document.getElementById("resultat-mul").innerHTML = mul;
 }
 
+
+function add1(){
+	// test de la list
+	//let num1num2;
+	console.log("work")
+	let num1 = document.getElementById("getToNumbersus11").value; 
+	let num2 = document.getElementById("getToNumbersus22").value; 
+	let add1 = +num1 + +num2;
+	document.getElementById("resultat-add1").innerHTML = add1;
+	
+	
+
+}
+function sus1(){
+	// test de la list
+	//let num1num2;
+	console.log("work")
+	let num1 = document.getElementById("getToNumbersus11").value; 
+	let num2 = document.getElementById("getToNumbersus22").value; 
+	let sus1 = +num1 - +num2;
+	document.getElementById("resultat-sus1").innerHTML = sus1;
+}
+function mul1(){
+	// test de la list
+	//let num1num2;
+	console.log("work")
+	let num1 = document.getElementById("getToNumbersus11").value; 
+	let num2 = document.getElementById("getToNumbersus22").value; 
+	let mul1 = +num1 * +num2;
+	document.getElementById("resultat-mul1").innerHTML = mul1;
+}
+function div1(){
+	// test de la list
+	//let num1num2;
+	console.log("work")
+	let num1 = document.getElementById("getToNumbersus11").value; 
+	let num2 = document.getElementById("getToNumbersus22").value; 
+	let div1 = +num1 / +num2;
+	document.getElementById("resultat-div1").innerHTML = div1;
+}
